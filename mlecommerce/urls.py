e2b1116e92367e967b3f0f21f90a89b07f8a90c3
AnshMittal1811 <anshm18111996@gmail.com>
2023-07-01 15:21:03 -0700

    User Register Form

--- mlecommerce/urls.py
+++ mlecommerce/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("core.urls"))
+    path("", include("core.urls")),
+    path("user/", include("userauths.urls")),
+
 ]
 
 if settings.DEBUG:
