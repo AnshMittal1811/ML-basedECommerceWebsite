ab1a97525b2d83417080c91b6090c0cde6239204
AnshMittal1811 <anshm18111996@gmail.com>
2023-07-08 15:58:46 -0700

    Customized Ranged Slider for E-commerce

--- core/context_processor.py
+++ core/context_processor.py
@@ -1,8 +1,12 @@
 from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, wishlist, Address
+from django.db.models import Min, Max
 
 def default(request):
     categories = Category.objects.all()
     vendors = Vendor.objects.all()
+
+    min_max_price = Product.objects.aggregate(Min("price"), Max("price"))
+
     try:
         address = Address.objects.get(user=request.user)
     except:
@@ -11,4 +15,5 @@ def default(request):
         'categories': categories,
         'address': address,
         'vendors': vendors,
+        'min_max_price': min_max_price,
     }
